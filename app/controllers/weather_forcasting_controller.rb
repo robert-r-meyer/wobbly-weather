fbca7f8fad31092b1b8379120b9ff99b34f829dd
Robert R. Meyer <20780405+robert-r-meyer@users.noreply.github.com>
2025-05-22 21:37:21 +0000

    Render forcast pretty and add weather icons

--- app/controllers/weather_forcasting_controller.rb
+++ app/controllers/weather_forcasting_controller.rb
@@ -1,2 +1,23 @@
+# frozen_string_literal: true
+
+# typed: false
+
 class WeatherForcastingController < ApplicationController
+    def index
+      # Default to New York if no location is provided
+      location = params[:location] || "New York"
+
+      # Fetch api key from environment variable for security
+      api_key = ENV["WEATHER_API_KEY"]
+      api = WeatherFetcher.new(api_key)
+
+      # Fetch weather data for the given location
+      city_forcast = api.weather_by_location(location)
+
+      # Determine if the location is cached
+      @cached_location = api.location_cached?(location)
+
+      # Handle error if the API call fails
+      @forecast = city_forcast unless city_forcast["error"]
+    end
 end
--- app/services/weather_fetcher.rb
+++ app/services/weather_fetcher.rb
@@ -2,6 +2,8 @@
 
 # type: false
 
+# WeatherFetcher is a service class that fetches weather data from the OpenWeatherMap API.
+# Cribed off of https://medium.com/@bamnelkarvivek/how-i-made-a-simple-weather-app-in-ruby-on-rails-309cdced25aa
 class WeatherFetcher
   include HTTParty
 
@@ -11,12 +13,32 @@ class WeatherFetcher
     @api_key = api_key
   end
 
-  def fetch_weather(city)
-    response = self.class.get("", query: { q: city, appid: @api_key })
-    if response.success?
-      response.parsed_response
-    else
-      raise "Error fetching weather data: #{response.message}"
+  # :reek:UtilityFunction
+  def location_cached?(city)
+    # Check if the cache exists for a given city
+    # City name only
+    cached = Rails.cache.exist?("weather_location/#{city}")
+    Rails.logger.info("Cache for #{city} exists: #{cached}")
+
+    cached
+  end
+
+  # :reek:FeatureEnvy
+  def weather_by_location(city)
+    # Fetch weather data for a given city
+    # City name only
+    city = city.split(",")[0]
+
+    # Fetch weather data from OpenWeatherMap API
+    # and store it in the cache for 1 hour
+    Rails.cache.fetch("weather_location/#{city}", expires_in: 30.minutes) do
+      response = self.class.get("", query: { q: city, appid: @api_key, units: "metric" })
+      if response.success?
+        response.parsed_response
+      else
+        Rails.logger.error("Failed to fetch weather data for #{city}: #{response.code} - #{response.message}")
+        { "error" => "Unable to fetch weather data for #{city}" }
+      end
     end
   end
 end
